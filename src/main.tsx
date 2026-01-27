6c78e19560410a6de0ad345bbebd3464116449a7
HuanCanhCut <tronghuanxxx@gmail.com>
2026-01-27 14:08:27 +0700

    feat: config redux persist with redux toolkit

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,15 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import GlobalStyles from './components/GlobalStyles/GlobalStyles.tsx'
 import { Provider } from 'react-redux'
-import { store } from './redux/store.ts'
+import { persistor, store } from './redux/store.ts'
+import { PersistGate } from 'redux-persist/integration/react'
 
 createRoot(document.getElementById('root')!).render(
     <GlobalStyles>
         <Provider store={store}>
-            <App />
+            <PersistGate persistor={persistor}>
+                <App />
+            </PersistGate>
         </Provider>
     </GlobalStyles>,
 )
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,13 +1,32 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import { FLUSH, PAUSE, PERSIST, persistReducer, persistStore, PURGE, REGISTER, REHYDRATE } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import userReducer from './reducers/userReducer'
 
-export const store = configureStore({
-    reducer: {
-        user: userReducer,
-    },
+const persistConfig = {
+    key: 'root',
+    version: 1,
+    storage,
+    whitelist: ['user'],
+}
+
+const rootReducer = combineReducers({
+    user: userReducer,
 })
 
-// Infer the `RootState` and `AppDispatch` types from the store itself
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
+})
+
+export const persistor = persistStore(store)
+
 export type RootState = ReturnType<typeof store.getState>
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch
