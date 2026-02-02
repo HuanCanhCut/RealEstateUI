3a66c27afa55fa5eebcb23fb716d6b2f5a5316dc
HuanCanhCut <tronghuanxxx@gmail.com>
2026-02-02 15:10:57 +0700

    refactor: update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -3,43 +3,41 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
-import prettier from 'eslint-plugin-prettier'
+import prettierPlugin from 'eslint-plugin-prettier'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
 
 export default defineConfig([
     globalIgnores(['dist']),
-
     {
         files: ['**/*.{ts,tsx}'],
-
+        extends: [
+            js.configs.recommended,
+            tseslint.configs.recommended,
+            reactHooks.configs.flat.recommended,
+            reactRefresh.configs.vite,
+        ],
         languageOptions: {
             ecmaVersion: 2020,
             globals: globals.browser,
         },
-
         plugins: {
-            prettier,
+            prettier: prettierPlugin,
+            'simple-import-sort': simpleImportSort,
         },
-
         rules: {
-            ...js.configs.recommended.rules,
-            ...tseslint.configs.recommended.rules,
-            ...reactHooks.configs.flat.recommended.rules,
-            ...reactRefresh.configs.vite.rules,
+            // ===== Prettier =====
+            'prettier/prettier': 'warn',
 
-            // Prettier
-            'prettier/prettier': [
+            'simple-import-sort/imports': [
                 'warn',
                 {
-                    tabWidth: 4,
-                    printWidth: 120,
-                    semi: false,
-                    singleQuote: true,
-                    arrowParens: 'always',
-                    plugins: ['prettier-plugin-tailwindcss'],
+                    groups: [['^react', '^next', '^\\w'], ['^']],
                 },
             ],
+            'simple-import-sort/exports': 'warn',
 
+            // ===== TS =====
             '@typescript-eslint/no-explicit-any': 'off',
             '@typescript-eslint/no-unused-vars': [
                 'warn',
